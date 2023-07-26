d0cf706f3044da227f2624b287e30907cb942ba0
niccoloruberti <nikiruberti@gmail.com>
2023-07-26 17:27:59 +0200

    esercizio completato

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,29 @@
 </head>
 
 <body>
-    
+    <div class="container">
+        <h1 class="my-5">Treni della giornata</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Codice Treno</th>
+                    <th scope="col">Partenza</th>
+                    <th scope="col">Arrivo</th>
+                    <th scope="col">In Orario</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($trains as $train)
+                <tr>
+                    <th scope="row">{{ $train->codice_treno}}</th>
+                    <td>{{ $train->stazione_di_partenza. ' - ' .$train->orario_di_partenza}}</td>
+                    <td>{{ $train->stazione_di_arrivo. ' - ' .$train->orario_di_arrivo}}</td>
+                    <td><i class="{{$train->in_orario ? 'fas fa-check' : 'fas fa-x'}}"></i></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
