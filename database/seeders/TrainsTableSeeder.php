b5ea8f3c0e363af8cac64df00e2d8b42a5acf7a8
niccoloruberti <nikiruberti@gmail.com>
2023-07-27 17:07:52 +0200

    esercizio completato

---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newTrain = new Train();
+            $newTrain->azineda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->word();
+            $newTrain->stazione_di_arrivo = $faker->word();
+            $newTrain->orario_di_partenza = $faker->time();
+            $newTrain->orario_di_arrivo = $faker->time();
+            $newTrain->codice_treno = $faker->bothify('######');
+            $newTrain->n_carrozze = $faker->randomNumber(2, false);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->data_di_partenza = $faker->date();
+
+            $newTrain->save();
+        }
+    }
+}
